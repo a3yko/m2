e0a9939485a91d2860e14d0d5cf5a7e9cbe6b961
Adrian Atanasov <adriansatanasov@gmail.com>
2021-02-25 23:09:38 -0600

    github add

---
+++ Program1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace Program1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            int num;
+
+            Console.WriteLine("Please enter an integer:");
+            input = Console.ReadLine();
+            num = Int32.Parse(input);
+            int[] numarr = new int[num];
+            Program1 p = new Program1();
+            int n = 0;
+            for (int i = 2; i < num; i++) {
+                if (p.numChecker(i))
+                {
+                    numarr[n] = i;
+                    n++;
+                }
+            }
+
+            int k = 0;
+            do
+            {
+                Console.Write(numarr[k] + " ");
+                k++;
+            } while (numarr[k] != 0);
+        }
+
+
+        public bool numChecker(int num){
+            if (num < 2)
+                return false;
+            if(num == 2)
+            {
+                return true;
+            }
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
+
+
---
+++ Program2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Program2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Size:");
+            String line;
+
+            int size;
+
+            line = Console.ReadLine();
+            size = Int32.Parse(line);
+            String[] values = new String[size];
+            float[] vals = new float[size];
+            Console.WriteLine("Please Enter indices:");
+            line = Console.ReadLine();
+            values = line.Split(' ');
+
+            for (int i = 0; i < size; i++)
+                vals[i] = Single.Parse(values[i], System.Globalization.NumberStyles.Float);
+
+            Program2 p = new Program2();
+            p.sort(vals);
+
+
+            for (int i = 0; i < size; i++)
+                Console.Write(vals[i] + " ");
+        }
+
+        public void sort(float[] arr)
+        {
+            float temp;
+            for (int k = 0; k <= arr.Length - 1; k++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if(arr[i].CompareTo(arr[i + 1]) > 0)
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
